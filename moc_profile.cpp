c69dd6cba95c13a0a63c06fc24dc78a378fc0390
Your Name <andy.leadbetter@theleadbetters.com>
2010-12-19 11:05:31 +0000

    clean up modify slider to qt-components version

---
+++ moc_profile.cpp
@@ -0,0 +1,98 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'profile.h'
+**
+** Created: Sun Dec 19 09:32:05 2010
+**      by: The Qt Meta Object Compiler version 62 (Qt 4.7.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "profile.h"
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'profile.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 62
+#error "This file was generated using the moc from 4.7.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+static const uint qt_meta_data_Profile[] = {
+
+ // content:
+       5,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       2,   14, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // properties: name, type, flags
+      16,    8, 0x0a095001,
+      19,    8, 0x0a095001,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_Profile[] = {
+    "Profile\0QString\0id\0name\0"
+};
+
+const QMetaObject Profile::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_Profile,
+      qt_meta_data_Profile, 0 }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &Profile::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *Profile::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *Profile::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_Profile))
+        return static_cast<void*>(const_cast< Profile*>(this));
+    return QObject::qt_metacast(_clname);
+}
+
+int Profile::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    
+#ifndef QT_NO_PROPERTIES
+     if (_c == QMetaObject::ReadProperty) {
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: *reinterpret_cast< QString*>(_v) = getId(); break;
+        case 1: *reinterpret_cast< QString*>(_v) = getName(); break;
+        }
+        _id -= 2;
+    } else if (_c == QMetaObject::WriteProperty) {
+        _id -= 2;
+    } else if (_c == QMetaObject::ResetProperty) {
+        _id -= 2;
+    } else if (_c == QMetaObject::QueryPropertyDesignable) {
+        _id -= 2;
+    } else if (_c == QMetaObject::QueryPropertyScriptable) {
+        _id -= 2;
+    } else if (_c == QMetaObject::QueryPropertyStored) {
+        _id -= 2;
+    } else if (_c == QMetaObject::QueryPropertyEditable) {
+        _id -= 2;
+    } else if (_c == QMetaObject::QueryPropertyUser) {
+        _id -= 2;
+    }
+#endif // QT_NO_PROPERTIES
+    return _id;
+}
+QT_END_MOC_NAMESPACE
